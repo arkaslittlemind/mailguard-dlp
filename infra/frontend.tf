5003800bdc654d56683dd62f0b7141bab677e4a5
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2026-08-16 23:54:10 +0530

    feat: S3 + CloudFront

---
+++ infra/frontend.tf
@@ -0,0 +1,72 @@
+resource "aws_s3_bucket" "web" {
+  bucket = var.web_bucket_name
+}
+
+resource "aws_cloudfront_origin_access_control" "web" {
+  name                              = "${var.project}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "web" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name              = aws_s3_bucket.web.bucket_regional_domain_name
+    origin_id                = "s3-web"
+    origin_access_control_id = aws_cloudfront_origin_access_control.web.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-web"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6" # AWS "CachingOptimized"
+  }
+
+  # SPA fallback: let Vue Router handle unknown paths
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+  viewer_certificate { cloudfront_default_certificate = true }
+}
+
+# Allow only this CloudFront distribution to read the bucket
+data "aws_iam_policy_document" "web" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.web.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.web.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "web" {
+  bucket = aws_s3_bucket.web.id
+  policy = data.aws_iam_policy_document.web.json
+}
+
+output "web_url" {
+  value = "https://${aws_cloudfront_distribution.web.domain_name}"
+}
